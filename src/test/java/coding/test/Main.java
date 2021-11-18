e33e59ce730600e249ae2fa09ab95fb0f24679c6
worjs320 <worjs320@naver.com>
2021-11-19 00:01:58 +0900

    Programmers: 해시 > 전화번호 목록 풀이

--- src/test/java/coding/test/Main.java
+++ src/test/java/coding/test/Main.java
@@ -1,7 +1,10 @@
 package coding.test;
 
+import coding.test.programmers.해시.전화번호_목록.Solution;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new String[]{"119", "97674223", "1195524421"}));
     }
 }
