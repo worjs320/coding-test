e060b46956c57ff925b845bb9640709e7f3a3cf8
jgkim <jgkim@sgtechi.co.kr>
2022-01-19 10:07:15 +0900

    Baekjoon: 다이나믹 프로그래밍 > 1,2,3 더하기

---
+++ src/main/java/coding/test/baekjoon/다이나믹_프로그래밍/일_이_삼_더하기/Main.java
@@ -0,0 +1,31 @@
+package coding.test.baekjoon.다이나믹_프로그래밍.일_이_삼_더하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        dp = new Integer[12];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
