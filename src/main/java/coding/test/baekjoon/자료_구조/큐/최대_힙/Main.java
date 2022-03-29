b9152d56f4a4dfc36fdc45536914e6dccbc0b8f0
jgkim <jgkim@sgtechi.co.kr>
2022-03-29 10:19:01 +0900

    Baekjoon: 큐 > 최대 힙

---
+++ src/main/java/coding/test/baekjoon/자료_구조/큐/최대_힙/Main.java
@@ -0,0 +1,31 @@
+package coding.test.baekjoon.자료_구조.큐.최대_힙;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                queue.add(num);
+            } else {
+                int outNum = queue.peek() == null ? 0 : queue.poll();
+                sb.append(outNum).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
