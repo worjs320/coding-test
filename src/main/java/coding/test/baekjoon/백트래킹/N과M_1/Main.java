d3c90d3e0dd51be2256d8c21a8703004b8c15b21
jgkim <jgkim@sgtechi.co.kr>
2022-04-05 09:09:06 +0900

    Baekjoon: 백트래킹 > N과 M 1

---
+++ src/main/java/coding/test/baekjoon/백트래킹/N과M_1/Main.java
@@ -0,0 +1,49 @@
+package coding.test.baekjoon.백트래킹.N과M_1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[N + 1];
+        arr = new int[M + 1];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < N; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i;
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
